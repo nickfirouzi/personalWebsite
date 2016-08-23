847362a2bc5b8790778f44b8d099eb21a75d16ea
Nick Firouzi <Nick.Firouzi@omegapoint.se>
2016-08-23 10:40:49 +0200

    Fix problem with cloudword.

--- html/script.js
+++ html/script.js
@@ -1,39 +1,54 @@
 var fill = d3.scale.category20();
 
 
-
-d3.csv("data.csv", function(data) {
-    data.forEach(function(d) {
+d3.csv("data.csv", function (data) {
+    data.forEach(function (d) {
         d.size = +d.size;
     });
 
     d3.layout.cloud().size([1000, 500])
         .words(data)
         .padding(5)
-        .rotate(function() { return Math.floor((Math.random() * 60) + 1); })
+        .rotate(function () {
+            return Math.floor((Math.random() * 60) + 1);
+        })
         .font("Impact")
-        .fontSize(function(d) { return d.size; })
+        .fontSize(function (d) {
+            return d.size;
+        })
         .on("end", draw)
         .start();
 
     function draw(words) {
         d3.select(".SkillCloud").append("svg")
             .attr("width", window.innerWidth)
-            .attr("height", 600)
+            .attr("height", 500)
             .append("g")
-            .attr("transform", "translate("+(window.innerWidth-800)+",250)")
+            .attr("transform", "translate(" + (window.innerWidth) / 3 + ",250)")
             .selectAll("text")
             .data(data)
             .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("fill", function(d, i) { return fill(i); })
+            .style("font-size", function (d) {
+                return d.size + "px";
+            })
+            .style("fill", function (d, i) {
+                return fill(i);
+            })
             .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
+            .attr("transform", function (d) {
                 return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
             })
-            .text(function(d) { return d.text; });
+            .text(function (d) {
+                return d.text;
+            });
     }
 });
+
+
+
+
+
+
 
 
 
